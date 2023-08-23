e4feb74640298d8cdf29241e563e142e95a8b49c
ECastro08 <worldmechatronics2023@gmail.com>
2023-08-23 17:49:24 -0500

    nuevo ejemplo de clases

--- src/es6/09-clases.js
+++ src/es6/09-clases.js
@@ -46,3 +46,67 @@ gato.caminar(); // Retorna '¡Caminando!, la la la'
 // Usamos los getter para obtener los valores de los atributos y los setters para reasignarlos.
 perro.getEdad; // Retorna 3
 gato.setEdad = 3; // Cambia su atributo edad a 3
+
+
+
+//Se declara la clase:
+
+class NombredelaClase {
+    //atributos y metodos
+}
+
+//Se crea una instancia (objeto), de la clase:
+
+const NuevoObjeto = new NombredelaClase();
+
+
+/// EJEMPLO MIO 
+
+class auto {
+
+    // atributos
+    constructor(modelo, cilindraje , color) {
+		this.modelo = modelo;  
+		this.cilindraje = cilindraje 
+		this.color = color;
+	}
+
+    // metodos
+    acelerar() {
+        return 'brummmmmm'
+    }
+    frenar() {
+        return 'chiiiiiii'
+    }
+    embragar() {
+        return 'embrague'
+    }
+
+// set y get 
+
+get getColor() {
+    return this.color;
+}
+
+set setColor(newColor) {
+    this.color = newColor;
+}
+
+};
+
+
+// Instanciamos la clase
+
+const newAuto = new auto();
+acelerar = newAuto.acelerar();
+embragar = newAuto.embragar();
+frenar = newAuto.frenar();
+
+console.log(acelerar, frenar, embragar);
+
+const auto1 = new auto(2019, 1600, 'rojo');
+const auto2 = new auto(2023, 1400, 'azul oscuro');
+
+auto1.acelerar();
+
+auto1.get
