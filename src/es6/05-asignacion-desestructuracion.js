60a7a59bcbb23f7336b688ba631ee0d8c4525a88
ECastro08 <worldmechatronics2023@gmail.com>
2023-08-23 09:16:45 -0500

    asignacion de desestructuracion

---
+++ src/es6/05-asignacion-desestructuracion.js
@@ -0,0 +1,12 @@
+// arrays destructuring
+
+let fruits = ['manzana','banano', 'pera'];
+let [a, b, c] = fruits;
+console.log(a, b, c);
+
+// Object destructuring
+
+let user = {username: 'ECastro', pais: 'Colombia'}
+let {username, pais} = user;
+console.log(username, user.pais);
+
