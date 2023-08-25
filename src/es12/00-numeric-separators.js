de587d6394043bfadf3f9b9a4b972845a27dffff
ECastro08 <worldmechatronics2023@gmail.com>
2023-08-25 11:44:30 -0500

    es12 se crearon los archivos 00 y 01 para los separadores numericos(_) y el replace

---
+++ src/es12/00-numeric-separators.js
@@ -0,0 +1,5 @@
+// creamos un valor numerico 
+const value = 1000000000000;      // sin el separador
+const valu3 = 1_000_000_000_000;  // con el operador _
+console.log(value);
+console.log(valu3);
---
+++ src/es12/01-replaceall.js
@@ -0,0 +1,6 @@
+//
+const string = "JavaScript es un maravilloso lenguaje de  programacion";
+
+const replacedString = string.replace("JavaScript", "TypeScript");
+                                // cambiamos JavaScript por TypeScript con el replace
+console.log(replacedString);
